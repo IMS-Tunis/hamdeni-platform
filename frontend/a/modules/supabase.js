ef6a9eb12a584ac2ba633f01526c8a1d7433900a
IMS-Tunis <chams-eddine.hemdani@tn.maarifschools.org>
2025-06-30 11:38:10 +0100

    authentification fix 1

--- frontend/a/modules/supabase.js
+++ frontend/a/modules/supabase.js
@@ -16,14 +16,15 @@ export function initializeLogin() {
 
       fetch("https://tsmzmuclrnyryuvanlxl.supabase.co/rest/v1/students?select=*&username=eq." + username + "&password=eq." + password, {
         headers: {
-          apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzbXptdWNscm55cnl1dmFubHhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3MzM5NjUsImV4cCI6MjA2MzMwOTk2NX0.-l7Klmp5hKru3w2HOWLRPjCiQprJ2pOjsI-HPTGtAiw"
+          apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
         }
       })
       .then(res => res.json())
       .then(data => {
         if (data.length === 1) {
-          localStorage.setItem("student_id", data[0].student_id);
-          localStorage.setItem("student_name", data[0].student_name);
+          localStorage.setItem("student_id", data[0].studentid);
+          localStorage.setItem("student_name", data[0].username);  // ✅ FIXED
+          localStorage.setItem("platform", data[0].platform);
           location.reload();
         } else {
           alert("Login failed. Check your credentials.");
