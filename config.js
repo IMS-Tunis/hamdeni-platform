0db68b649133ac1c06bdce44bc4ea20451649f08
IMS-Tunis <chams-eddine.hemdani@tn.maarifschools.org>
2025-07-06 01:16:41 +0100

    update Supabase URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://ims-tunis.supabase.co",
+  SUPABASE_URL: "https://tsmzmuclrnyryuvanlxl.supabase.co",
   SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzbXptdWNscm55cnl1dmFubHhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3MzM5NjUsImV4cCI6MjA2MzMwOTk2NX0.-l7Klmp5hKru3w2HOWLRPjCiQprJ2pOjsI-HPTGtAiw",
   TEACHER_PASSWORD: "wxcv"
 };
